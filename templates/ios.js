c6798d558c0ac08523a2a9a8da66f6cd15e25a68
Michael Schneider <maicki@users.noreply.github.com>
2018-03-02 19:51:23 -0800

    Fix import in ios template of RCTBridgeModule (#50)

--- templates/ios.js
+++ templates/ios.js
@@ -30,10 +30,10 @@ end
 
   name: ({ name }) => `${platform}/${name}.h`,
   content: ({ name }) => `
-#if __has_include("RCTBridgeModule.h")
-#import "RCTBridgeModule.h"
-#else
+#if __has_include(<React/RCTBridgeModule.h>)
 #import <React/RCTBridgeModule.h>
+#else
+#import "RCTBridgeModule.h"
 #endif
 
 @interface ${name} : NSObject <RCTBridgeModule>
